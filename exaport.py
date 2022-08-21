ecf83d0e2a71eaf71c84bdebe50335c04722c3b8
Utkarsh Yadav <38191717+ViperX7@users.noreply.github.com>
2022-08-21 08:06:09 +0530

    added configurables script starts and loads neovim by itself

--- exaport.py
+++ exaport.py
@@ -2,20 +2,29 @@
 Code to export vim screen exactly to HTML
 """
 
-import tkinter
+import subprocess
 
 import pyautogui as gui
 import pyperclip
 
-gui.hotkey("alt", "\t")
-gui.typewrite("zR")
-gui.typewrite(
-    ":set nocursorcolumn nocursorline colorcolumn=10000 list! nornu! nonumber\n"
-)
-gui.typewrite(":IndentBlanklineToggle\n")
+
+def send_lines(keylist):
+    """
+    Press the supplied
+    """
+    if isinstance(keylist, str):
+        gui.typewrite(keylist + "\n")
+    elif isinstance(keylist, list):
+        cmd = "\n".join(keylist)
+        send_lines(cmd)
+    else:
+        raise TypeError()
 
 
 def snap_from_term():
+    """
+    Copy's the text visible on the terminal screen
+    """
     gui.hotkey("ctrl", "|")
     gui.hotkey("ctrl", "}")
     clipboard_content = pyperclip.paste()
@@ -23,48 +32,61 @@ def snap_from_term():
 
 
 def get_term_output(ad_start=0, ad_end=None):
+    """
+    Term output Processor for Neorg on nvim
+    """
     buffer_dump = []
 
     old_clipboard_content = ""
-    clipboard_content = "1"
-
     while (clipboard_content := snap_from_term()) != old_clipboard_content:
-        # # Copy everything on screen gnome-terminal screen
+        #  Copy everything on screen gnome-terminal screen
         clipboard_content = clipboard_content.split("\n")
         clipboard_content = clipboard_content[1:-3][ad_start:ad_end]
         clipboard_content = "\n".join(clipboard_content)
 
         if old_clipboard_content == clipboard_content:
-            return buffer_dump
+            buffer_dump.pop()
+            break
         buffer_dump.append(clipboard_content)
         old_clipboard_content = clipboard_content
         gui.hotkey("pagedown")
-        print("lc")
-    print("X")
+    return buffer_dump
 
 
-def squash_common(data: list):
-    result = []
-    for i, entry in enumerate(data):
-        if i < (len(data) - 1):
-            for lines in entry.split("\n"):
-                print(lines)
+# def squash_common(data: list):
+#     result = []
+#     for i, entry in enumerate(data):
+#         if i < (len(data) - 1):
+#             for lines in entry.split("\n"):
+# print(lines)
 
+# Configurables
+BGCOLOR = "#282C34"
+FILE_PATH = "../torch_1.norg"
+TERM_START_CMD = ["gnome-terminal", "--", "nvim", FILE_PATH]
+START_DELAY = 1
 
-squash_common(
-    [
-        "okokokok",
-        "okokoktok",
-        "tokokoktok",
-    ]
-)
+# Start the terminal
+out = subprocess.run(TERM_START_CMD, check=True)
+gui.sleep(START_DELAY)
 
+# Conf Features
+vim_cmds = [
+    ":set nocursorcolumn nocursorline colorcolumn=10000 list! nornu! nonumber",
+    ":IndentBlanklineToggle",
+    "zR",
+]
+
+send_lines(vim_cmds)
+gui.sleep(START_DELAY)
+
+# post processing
 bufcnt = get_term_output(0, -2)
 bufcnt = "\n".join(bufcnt)
 bufcnt = [
     "<pre>",
     "<center>",
-    "<div style='background-color:#282C34;line-height:12pt'>",
+    f"<div style='background-color:{BGCOLOR};line-height:12pt'>",
     bufcnt,
     "</div>",
     "</center>",
@@ -74,4 +96,5 @@ bufcnt = "\n".join(bufcnt)
 with open("test.md", "w") as file:
     file.write(bufcnt)
 
-gui.hotkey("alt", "\t")
+cmdlst = [":qall!", "exit"]
+send_lines(cmdlst)

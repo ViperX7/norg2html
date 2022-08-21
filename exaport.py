b210edc32a032789de76bd7f30a7d447aaffff9d
Utkarsh Yadav <38191717+ViperX7@users.noreply.github.com>
2022-08-21 11:35:45 +0530

    Added Argument parser and fix duplicate last page permanently

--- exaport.py
+++ exaport.py
@@ -2,6 +2,7 @@
 Code to export vim screen exactly to HTML
 """
 
+import argparse
 import subprocess
 import tempfile
 from os import remove
@@ -9,6 +10,7 @@ from shutil import copy
 
 import pyautogui as gui
 import pyperclip
+from bs4 import BeautifulSoup
 
 
 def send_lines(keylist):
@@ -34,6 +36,19 @@ def snap_from_term():
     return clipboard_content
 
 
+def find_match(slst):
+    """
+    Magic
+    """
+    slst = BeautifulSoup(slst, "lxml")
+    slst = slst.text.split("\n")
+    # __import__("pprint").pprint(slst)
+
+    if slst[-1][0] == "~" and slst[-1].strip(" ") == "~":
+        return True
+    return False
+
+
 def get_term_output(ad_start=0, ad_end=None):
     """
     Term output Processor for Neorg on nvim
@@ -42,29 +57,34 @@ def get_term_output(ad_start=0, ad_end=None):
 
     old_clipboard_content = ""
     while (clipboard_content := snap_from_term()) != old_clipboard_content:
+
+        old_clipboard_content = clipboard_content
         #  Copy everything on screen gnome-terminal screen
         clipboard_content = clipboard_content.split("\n")
         clipboard_content = clipboard_content[1:-3][ad_start:ad_end]
         clipboard_content = "\n".join(clipboard_content)
 
-        if old_clipboard_content == clipboard_content:
-            if buffer_dump[-1] == buffer_dump[-2]:
-                buffer_dump.pop()
-            elif CUT_FIX:
-                buffer_dump.pop()
-            break
         buffer_dump.append(clipboard_content)
-        old_clipboard_content = clipboard_content
         gui.hotkey("pagedown")
+
+    dmp = buffer_dump.pop() if find_match(buffer_dump[-2]) else None
+    dmp = buffer_dump.pop() if find_match(buffer_dump[-2]) else None
+    dmp = buffer_dump.pop() if find_match(buffer_dump[-2]) else dmp
+
     return buffer_dump
 
 
-def export_norg():
+def export_norg(file_path, out_path=None):
+    """
+    Handler
+    """
+    out_path = out_path if out_path else file_path.replace("norg", "md")
+
     # Create a copy of original norg file
-    copy(FILE_PATH, TMP_FILE_PATH)
+    copy(file_path, TMP_FILE_PATH)
 
     # Start the terminal
-    out = subprocess.run(TERM_START_CMD, check=True)
+    subprocess.run(TERM_START_CMD, check=True)
     gui.sleep(START_DELAY)
 
     # Conf Features
@@ -76,9 +96,8 @@ def export_norg():
 
     excmds = [":Neorg presenter start"]
     vim_cmds = excmds + vim_cmds if ENABLE_PRESENTER else vim_cmds
-
     send_lines(vim_cmds)
-    gui.sleep(START_DELAY)
+    gui.sleep(PROCESS_DELAY)
 
     # post processing
     bufcnt = get_term_output() if ENABLE_PRESENTER else get_term_output(0, -2)
@@ -95,7 +114,8 @@ def export_norg():
         "</pre>",
     ]
     bufcnt = "\n".join(bufcnt)
-    with open("test.md", "w") as file:
+    print(out_path)
+    with open(out_path, "w", encoding="utf-8") as file:
         file.write(bufcnt)
 
     cmdlst = [":qall!", "exit"]
@@ -103,15 +123,29 @@ def export_norg():
     remove(TMP_FILE_PATH)
 
 
-BGCOLOR = "#282C34"
 _, TMP_FILE_PATH = tempfile.mkstemp(".norg")
-FILE_PATH = "../torch_1.norg"
 TERM_START_CMD = ["gnome-terminal", "--", "nvim", TMP_FILE_PATH]
-START_DELAY = 0.5
-ENABLE_PRESENTER = True
-CUT_FIX = not ENABLE_PRESENTER
-FONT = "Fira Code"
-
 KB_INJECT = "cat term.cfg | dconf load /org/gnome/terminal/legacy/profiles:/"
+
+ENABLE_PRESENTER = False
+START_DELAY = 1
+PROCESS_DELAY = 1
+BGCOLOR = "#282C34"
+FONT = "Fira Code"
+COLORSCHEME = None
+
 if __name__ == "__main__":
-    export_norg()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path")
+    parser.add_argument("--font", default=FONT, help="font to set for the output")
+    parser.add_argument("--bgcolor", default=BGCOLOR, help="background color to fill")
+    parser.add_argument("--colorscheme", default=None, help="specify vim colorscheme")
+    parser.add_argument("--presenter", default=False, help="Enable norg presenter mode")
+    args = parser.parse_args()
+
+    ENABLE_PRESENTER = args.presenter
+    BGCOLOR = args.bgcolor
+    FONT = args.font
+    COLORSCHEME = args.colorscheme
+
+    export_norg(args.path)
